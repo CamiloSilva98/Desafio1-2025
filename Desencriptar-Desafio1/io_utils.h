ca2a291832bac16c8eb0052669625137c16d2624
Camilo <Brayan.silva@udea.edu.co>
2025-09-22 10:40:34 -0500

    io_utils

--- Desencriptar-Desafio1/io_utils.h
+++ Desencriptar-Desafio1/io_utils.h
@@ -1,10 +1,10 @@
 #ifndef IO_UTILS_H
 #define IO_UTILS_H
+#include <cstddef>
 
-class io_utils
-{
-public:
-    io_utils();
-};
+unsigned char* leerArchivoEncriptado(const char* nombreArchivo, size_t& tamaño);
+char* leerFragmentoConocido(const char* nombreArchivo, size_t& longitud);
+bool guardarMensajeRecuperado(const char* nombreArchivo, const char* mensaje, size_t longitud);
+void liberarBuffer(void* buffer);
 
 #endif // IO_UTILS_H
